399274d3169529b4779dbab3f833f619e98de41e
Burssens Dominique <dominique.burssens@kdg.be>
2018-07-17 00:28:45 +0200

    bijna alle talen aangepast

--- mail/login.php
+++ mail/login.php
@@ -6,7 +6,7 @@ if(empty($_POST['prename'])      ||
    empty($_POST['email']))
    {
    echo "No arguments Provided!";
-   return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>byebye</title></head><body><script>window.location.href="index.html";</script></body></html>';
+   return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>byebye</title></head><body><script>window.location.href="../index.html";</script></body></html>';
    }
    
 $prename = strip_tags(htmlspecialchars($_POST['prename']));
@@ -57,11 +57,11 @@ if (isset($_POST['phone'])) {
 }
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'xxx@xxx.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website submition Form:  $name";
-$email_body = "You have received a new message from your website submission form.\n\n"."Here are the details:\n\nName: $prename $name\n\nEmail: $email\n\nref-link: $reflink\n\nSponsor:\n$sponsor"\n\nPhone:\n$phone"\n\nBirtdate:\n$sdate"\n\nPromote:\n$promote"\n\nCharity:\n$charity"\n\nBonus:\n$bonus";
+$email_body = "You have received a new message from your website submission form.\n\n.Here are the details:\n\nName: $prename $name\n\nEmail: $email\n\nref-link: $reflink\n\nSponsor:\n$sponsor\n\nPhone:\n$phone\n\nBirtdate:\n$sdate\n\nPromote:\n$promote\n\nCharity:\n$charity\n\nBonus:\n$bonus";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email";
 mail($to,$email_subject,$email_body,$headers);
-return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>byebye</title></head><body><script>window.location.href="index.html";</script></body></html>';
+return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>byebye</title></head><body><script>window.location.href="../index.html";</script></body></html>';
 ?>
